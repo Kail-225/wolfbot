2b0c6fe952d3737c4465a8c6b698d7f01e6218e3
Server_FI <kaileldennorth@gmail.com>
2026-05-24 10:10:33 +0300

    исправление2

--- messages.py
+++ messages.py
@@ -5,7 +5,7 @@ def messages(bot):
         match message.content_type:
             case "text":
                 if message.text=="/start@WoIlFbOt":
-                    await bot.send_message(message.chat.id,'Бот успешно добавлен в группу. Задайте правила для группы с помощью команды "Задать правила <Правила>"\nДля ознакомления с функционалом используйте команду "Команды"',reply_to_message_id=message.id)
+                    await bot.send_message(message.chat.id,'Бот успешно добавлен в группу. Задайте правила для группы с помощью команды "Задать правило <Правила>"\nДля ознакомления с функционалом используйте команду "Команды"',reply_to_message_id=message.id)
                 elif message.text[0:7]=="/start":
                     pass
                 elif message.from_user.first_name=="Telegram":
